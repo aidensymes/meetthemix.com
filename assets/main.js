4541c1b1793756f7c5ea94339203c928f14a9fe3
aidensymes <hello@aidensymes.com>
2019-03-13 16:04:23 -0400

    style soundcloud

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,18 @@
 var colors = ['#4F91CD', '#FF8400', '#FFCB02', '#FFFFFF'];
+var current;
 
 $(document).ready(function() {
   $('#remix--button').click(function() {
-    var ranNum = Math.floor(Math.random() * 4);
+    var ranNum = Math.floor(Math.random() * colors.length);
+    if (ranNum == current) {
+      if (ranNum == 0) {
+        ranNum += 1;
+      } else if (ranNum == colors.length - 1) {
+        ranNum -= 1;
+      }
+    }
+    current = ranNum;
     var newColor = colors[ranNum];
-    console.log(ranNum);
     $('html, body').css('background-color', newColor);
     $('.rule--light').css('border-color', newColor);
     $('.footer__social, h2, h1').css('color', newColor);
